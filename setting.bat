4a706f4558e6d4f00230ac4ab3575ab50d5d360e
Platform1117 <230233@naver.com>
2022-09-13 14:17:20 +0900

    초기 셋팅

--- setting.bat
+++ setting.bat
@@ -1,4 +1,4 @@
 python -m venv venv
-./Script/Activate.bat
+call %cd%\venv\Scripts\Activate.bat
 python.exe -m pip install --upgrade pip
 pip install opencv-python
---
+++ start.bat
@@ -0,0 +1,1 @@
+call %cd%\venv\Scripts\Activate.bat
