3cbf60697e2130203d8e35c6c972dc06af277424
Platform1117 <230233@naver.com>
2022-10-25 09:20:47 +0900

    과제 수정

--- hw1/Q2_Find_Dead_Pixel.py
+++ hw1/Q2_Find_Dead_Pixel.py
@@ -1,24 +1,30 @@
 import numpy as np
 import cv2
 
-red, green = (0,0,128), (0,128,0)
-chanel_BG = np.zeros((862,2526),np.uint8)
+red, green = (0, 0, 128), (0, 128, 0)
+
+
 def OpenImg(num):
     img_name = "images/DeadPixel" + str(num) + ".bmp"
     img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+    img2 = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
     img = np.float32(img)
     if img is None:
         raise Exception("파일 영상 처리 에러")
     else:
-        FindDeadPixel(img, num)
+        FindDeadPixel(img, num, img2)
 
-def FindDeadPixel(img, num):
+
+def FindDeadPixel(img, num, img2):
     totalDeadPixel = 0
     center_status = True
-    (b,g,r) = cv2.split(img)
-    b[:,:] =0
-    g[:,:] =0
+    (b, g, r) = cv2.split(img)
+    b[:, :] = 0
+    g[:, :] = 0
     mask = cv2.threshold(r, 0, 255, cv2.THRESH_BINARY)[1]
+    mask2 = cv2.threshold(img2, 0, 255, cv2.THRESH_BINARY)[1]
+
+    r2,c2 = mask2.shape
     (row, col) = mask.shape
     width = col // 3 + 1
     height = row // 3 + 1
@@ -34,20 +40,22 @@ def FindDeadPixel(img, num):
                 if deadPixel > 1:
                     center_status = False
             totalDeadPixel += deadPixel
-    mask = cv2.merge((b,g,mask))
+    mask = cv2.merge((b, g, mask))
     if totalDeadPixel < 10 and center_status:
         ShowImg(mask, 'GOOD', num)
     else:
-        ShowImg(mask,'NG',num)
+        ShowImg(mask, 'NG', num)
+
 
 def ShowImg(mask, status, num):
-    li = [chanel_BG,chanel_BG,mask]
     title = 'DeadPixel' + str(num) + '.bmp'
-    if status =='NG':
-        cv2.putText(mask,status, (2440,50), cv2.FONT_HERSHEY_SIMPLEX, 2, red)
+    if status == 'NG':
+        cv2.putText(mask, status, (2440, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, red)
     else:
-        cv2.putText(mask,status, (2350,50), cv2.FONT_HERSHEY_SIMPLEX, 2, green)
+        cv2.putText(mask, status, (2350, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, green)
     cv2.imshow(title, mask)
+
 
 for i in range(3):
     OpenImg(i+1)
--- hw1/Q3_Half_And_Half.py
+++ hw1/Q3_Half_And_Half.py
@@ -1,4 +1,3 @@
-from turtle import width
 import numpy as np
 import cv2
 
